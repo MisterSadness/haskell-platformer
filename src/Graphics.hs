fb867f5b4a7861ec0f0b522133c8d2cfbadbd3cb
MisterSadness <lewandowskipiotr98@gmail.com>
2020-06-22 20:49:40 +0200

    initial changes for displaying text

--- src/Graphics.hs
+++ src/Graphics.hs
@@ -2,9 +2,11 @@ module Graphics where
 
 import           GameObjects
 import qualified SDL
+import qualified SDL.Font
 import           Data.Colour
 import           Data.Colour.SRGB
 import           Data.Colour.Names
+import           Data.Text
 
 class Renderable a where
     render :: SDL.Renderer -> a -> IO ()
@@ -25,6 +27,19 @@ instance Renderable Game where
         setColour renderer darkmagenta >> render renderer player
         setColour renderer brown >> mapM_ (render renderer) (visibleObstacles player obstacles)
     render _ (Finished _) = undefined
+
+fontPath :: FilePath
+fontPath = "resources/UbuntuMono-B.ttf" 
+
+writeText :: SDL.Window -> Text -> IO ()
+writeText window str = do
+      font <- SDL.Font.load fontPath 50
+      text <- SDL.Font.solid font (SDL.V4 255 255 255 0) str
+      SDL.Font.free font
+      screen <- SDL.getWindowSurface window
+      SDL.surfaceBlit text Nothing screen Nothing
+      SDL.freeSurface text
+      SDL.updateWindowSurface window
 
 drawRectangle :: SDL.Renderer -> Position -> (Double, Double) -> IO ()
 drawRectangle renderer (px, py) (width, height) =
