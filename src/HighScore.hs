1993a40a13f564213cbaf625cb1709235e95c328
MisterSadness <lewandowskipiotr98@gmail.com>
2020-06-22 18:37:02 +0200

    some fixes to HighScore

--- src/HighScore.hs
+++ src/HighScore.hs
@@ -1,7 +1,6 @@
 module HighScore(updateScores) where
 
 import System.IO
-import Data.Functor
 import Text.Read(readMaybe)
 import Data.Maybe
 import System.Directory
@@ -16,17 +15,15 @@ updateScores :: Int -> IO ()
 updateScores score = do
     fileExists <- doesFileExist scoreFile
     fileAccesible <- checkPermission $ readable &&& writable >>> uncurry (&&)
-    
-    if fileExists && fileAccesible 
+
+    if fileExists && fileAccesible
         then do
             oldScores <- readScores
             writeScores $ insertScore score oldScores
-        else if not fileExists
-                then writeScores [score]
-                else return ()
+        else unless fileExists $ writeScores [score]
 
 checkPermission :: (Permissions -> Bool)-> IO Bool
-checkPermission pred = doesFileExist scoreFile >>= \e -> if e then pred <$> getPermissions scoreFile else return False
+checkPermission predicate = doesFileExist scoreFile >>= \e -> if e then predicate <$> getPermissions scoreFile else return False
 
 readFileWithoutClosing :: Handle -> IO [String]
 readFileWithoutClosing h = (hIsEOF h) >>= \b -> if not b then do
@@ -37,15 +34,15 @@ readFileWithoutClosing h = (hIsEOF h) >>= \b -> if not b then do
 readScores :: IO [Int]
 readScores = do
     handle <- openFile scoreFile ReadMode
-    scores <- fromMaybe [] <$> mapM readMaybe <$> readFileWithoutClosing handle
+    scores <- fromMaybe [] . mapM readMaybe <$> readFileWithoutClosing handle
     hClose handle
     return scores
 
 insertScore :: Int -> [Int] -> [Int]
 insertScore s [] = [s]
-insertScore s (x:xs) 
+insertScore s (x:xs)
     | x > s = x:insertScore s xs
     | otherwise = s:x:xs
 
 writeScores :: [Int] -> IO ()
-writeScores scores = withFile scoreFile WriteMode $ \handle -> mapM_ (\score -> hPutStrLn handle $ show score) scores
+writeScores scores = withFile scoreFile WriteMode $ \handle -> mapM_ (hPrint handle) scores
